894768b68158f19b4d1324a408348f18ff72aecd
Emerson Junqueira <emoonlit@gmail.com>
2023-11-11 16:17:11 -0300

    feat: added base api config with jwt

---
+++ AlfarBackendChallenge/AlfarBackendChallenge/Config/ApiConfig.cs
@@ -0,0 +1,26 @@
+namespace AlfarBackendChallenge.Config;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment environment)
+    {
+        // Configure the HTTP request pipeline.
+        if (environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthConfiguration();
+
+        return app;
+    }
+}
---
+++ AlfarBackendChallenge/AlfarBackendChallenge/Config/AppSettings.cs
@@ -0,0 +1,8 @@
+namespace AlfarBackendChallenge.Config;
+public class AppSettings
+{
+    public string Secret { get; set; }
+    public int ExpiresIn { get; set; }
+    public string Issuer { get; set; }
+    public string Audience { get; set; }
+}
